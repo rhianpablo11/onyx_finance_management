168c6bec32315da7e8e1c24c8d2b44afb3b99dfe
Rhian Pablo <rhianpablo11@gmail.com>
2026-01-18 02:18:49 -0300

    feat: added stylezeds versions of this components

--- front-end-app/src/components/wellcome.tsx
+++ front-end-app/src/components/wellcome.tsx
@@ -1,1 +1,21 @@
 // component for describes wellcome in initial page
+
+
+function Wellcome(){
+
+    return(
+        <>
+        <div className="flex w-full flex-col justify-center items-center">
+            <h1 className="text-white font-normal text-3xl">
+                Bem vindo, ao Onyx
+            </h1>
+            <h1 className="text-white font-extralight text-sm">
+                Gerenciamento financeiro descomplicado
+            </h1>
+
+        </div>
+        </>
+    )
+}
+
+export default Wellcome
--- front-end-app/src/pages/testingPage.tsx
+++ front-end-app/src/pages/testingPage.tsx
@@ -1,11 +1,13 @@
 //page for test of component conduct
 
 import Balance from "../components/balance"
+import Button from "../components/button"
 import ChatBubble from "../components/chatBubble"
 import HeaderInternal from "../components/headerInternal"
 import Input from "../components/input"
 import ListTransaction from "../components/listTransaction"
 import NavBar from "../components/navBar"
+import Wellcome from "../components/wellcome"
 
 
 function TestingPage(){
@@ -14,13 +16,16 @@ function TestingPage(){
     return (
         <>
             <div className="w-full m-0 px-4 flex-col bg-black justify-center items-center">
-                <HeaderInternal />
+                {/* <HeaderInternal />
                 <Balance />
                 <Input />
                 <ChatBubble />
                 <ListTransaction />
                 <ListTransaction />
-                <NavBar />
+                <NavBar /> */}
+                <Wellcome />
+                <Input />
+                <Button />
             </div>
         </>
     )
