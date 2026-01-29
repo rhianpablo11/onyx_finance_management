8df95234a1f9583dc3d1c62db268c5afd330f4c9
Rhian Pablo <rhianpablo11@gmail.com>
2026-01-29 00:05:17 -0300

    feat: added signout function

--- front-end-app/src/components/ui/headerInternal.tsx
+++ front-end-app/src/components/ui/headerInternal.tsx
@@ -1,8 +1,19 @@
 // component for header of internal pages -> say hello or indicate page
+import { useNavigate } from "react-router-dom"
 import type { HeaderInternalProps } from "../../interfaces/interfacesComponents"
+import { removeCookie } from "../../services/cookiesService"
+import { removeToken } from "../../services/tokenService"
 
 function HeaderInternal(props: HeaderInternalProps){
     const {type, legend, title, name, onClickChildren} = props
+    const navigate = useNavigate()
+
+    //function temporary for logout
+    const onClickLogout = () => {
+        removeCookie('user_name')
+        removeToken()
+        navigate('/')
+    } 
 
     if(type == 'wellcome'){
         return(
@@ -24,10 +35,14 @@ function HeaderInternal(props: HeaderInternalProps){
                             </div>
                         </div>
                         <div className="flex text-white items-center py-4">
-                            <button>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <button onClick={onClickLogout}>
+                                {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                                </svg> */}
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
                                 </svg>
+
                             </button>
                         </div>
                 </div>
