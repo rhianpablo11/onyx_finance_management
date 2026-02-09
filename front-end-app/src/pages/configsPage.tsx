9b5ef946994f32b386851d97b4c620ab10392c74
Rhian Pablo <rhianpablo11@gmail.com>
2026-02-09 02:39:24 -0300

    feat: add one more archive of previus commit

---
+++ front-end-app/src/pages/configsPage.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react"
+import AccountInfo from "../components/accountInfo"
+import GeneralSettings from "../components/generalSettings"
+import InfoApp from "../components/infoApp"
+
+
+function ConfigsPage(){
+    const [componentToShow, setComponentToShow] = useState('geral')
+    const onClickFather = async (buttonClicked:string) =>{
+        console.log(buttonClicked)
+        setComponentToShow(buttonClicked)
+    }
+
+    const renderInternal = () => {
+        if(componentToShow == 'geral'){
+            return(
+                <>
+                    <div className="flex mt-4">
+                        <GeneralSettings onClickChildren={onClickFather}/>
+                    </div>
+                    <div className="flex fixed w-full px-4 left-0 bottom-28">
+                        <InfoApp onClickChildren={onClickFather}/>
+                    </div>
+                </>
+            )
+        } else{
+            return(
+                <>
+                    <div className="flex mt-4">
+                        <GeneralSettings onClickChildren={onClickFather}/>
+                    </div>
+                    <div className="flex fixed w-full px-4 left-0 bottom-28">
+                        <InfoApp onClickChildren={onClickFather}/>
+                    </div>
+                </>
+            )
+        }
+    }
+
+    return(
+        <>
+            <div className="w-full flex flex-col">
+                <AccountInfo />
+                {renderInternal()}
+                
+            </div>
+        </>
+    )
+}
+
+export default ConfigsPage
