73eff54c0312879bcf966f4a567b0968149853ed
rhianpablo11 <rhianpablo11@gmail.com>
2026-03-14 19:48:29 -0300

    feat: added new archives from react-aria adobe for dataRangePicker

---
+++ front-end-app/src/components/react-aria/Dialog.tsx
@@ -0,0 +1,6 @@
+import { type DialogProps, Dialog as RACDialog } from 'react-aria-components';
+import { twMerge } from 'tailwind-merge';
+
+export function Dialog(props: DialogProps) {
+  return <RACDialog {...props} className={twMerge('outline box-border p-6 [[data-placement]>&]:p-4 max-h-[inherit] overflow-auto relative', props.className)} />;
+}
