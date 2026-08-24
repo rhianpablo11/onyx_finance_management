62d7da04d88e741a39f3b108eeeec9daaccd10d2
rhianpablo11 <rhianpablo11@gmail.com>
2026-08-24 09:03:42 -0300

    fix: fix navBar box

--- front-end-app/src/components/ui/navBar.tsx
+++ front-end-app/src/components/ui/navBar.tsx
@@ -3,40 +3,34 @@ import backgroundNavBar from '../../assets/bg-navBar2.svg?url'
 import backgroundChatButton from '../../assets/bg-buttonChat.svg?url'
 import type { NavBarProps } from '../../interfaces/interfacesComponents'
 
-
 function NavBar(props: NavBarProps){
     const {onClickButtonChildren, buttonSelected} = props
 
-    
-
     return (
         <>
-            <div className='w-full flex flex-col items-end'>
+            <div className='w-full relative pointer-events-none'>
+                
                 {buttonSelected == 'chat' ? (
                     null
                 ): (
                     <>
-                        <div>
+                        <div className='absolute bottom-full right-0 mb-1 pointer-events-auto'>
                             <button onClick={() => onClickButtonChildren('chat')}
-                                className=' ml-4 rounded-[33px] bg-linear-to-tl from-white/50 via-black to-white/50 p-px'>
-                                <div className={`h-16 w-16  shrink-0  rounded-4xl  flex items-center justify-center  bg-cover bg-center bg-no-repeat ${buttonSelected == 'chat' ? 'text-white' : 'text-white/50'}`} style={{backgroundImage: `url("${backgroundChatButton}")`}}>
-                                    {/* <svg width="40" height="40" stroke="currentColor" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M21.9956 12.0175C16.6323 12.3419 12.3399 16.6343 12.0156 21.9975H11.9756C11.6556 16.6343 7.36325 12.3419 2 12.0175V11.9775C7.36325 11.6576 11.6556 7.36521 11.98 2.00195H12.02C12.3444 7.36521 16.6367 11.6576 22 11.982V12.0175H21.9956Z" 
-                                            fill="#ffffff"/>
-                                    </svg> */}
+                                className='rounded-[33px] bg-linear-to-tl from-white/50 via-black to-white/50 p-px shadow-lg'>
+                                <div className={`h-16 w-16 shrink-0 rounded-4xl flex items-center justify-center bg-cover bg-center bg-no-repeat ${buttonSelected == 'chat' ? 'text-white' : 'text-white/50'}`} style={{backgroundImage: `url("${backgroundChatButton}")`}}>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
                                     </svg>
-
                                 </div>
                             </button>
                         </div>
                     </>
                 )}
                 
-                <div className="w-full flex justify-center items-center mb-4 mt-4 ">
+                {/* BARRA DE NAVEGAÇÃO INFERIOR */}
+                <div className="w-full flex justify-center items-center mb-4 mt-4 pointer-events-auto">
                     <div className="rounded-[21px] flex-1 bg-linear-to-tl from-white/50 via-black to-white/50 p-px">
-                        <div className="w-full h-14 flex  px-3  rounded-[20px] justify-between bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url("${backgroundNavBar}")`}}>
+                        <div className="w-full h-14 flex px-3 rounded-[20px] justify-between bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url("${backgroundNavBar}")`}}>
                             <button onClick={() => onClickButtonChildren('home')}
                                     className={buttonSelected == 'home' ? "text-white" : "text-white/50"}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
@@ -48,14 +42,12 @@ function NavBar(props: NavBarProps){
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                                 </svg>
-
                             </button>
                             <button onClick={() => onClickButtonChildren('insights')}
                                     className={buttonSelected == 'insights' ? "text-white" : "text-white/50"}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
                                 </svg>
-
                             </button>
                             <button onClick={() => onClickButtonChildren('addTransaction')}
                                     className={buttonSelected == 'addTransaction' ? "text-white" : "text-white/50"}>
@@ -77,10 +69,8 @@ function NavBar(props: NavBarProps){
                     </div>
                 </div>
             </div>
-            
         </>
     )
 }
-
 
 export default NavBar
